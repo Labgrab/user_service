edcd5d6b5f917610233ed18719375961e57499e9
Ademun <78663343+Ademun@users.noreply.github.com>
2026-01-08 00:21:09 +0300

    Config update

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -7,9 +7,19 @@ import (
 	"github.com/joho/godotenv"
 )
 
+type Environment string
+
+const (
+	Development Environment = "DEV"
+	Production  Environment = "PROD"
+)
+
 type Config struct {
-	Port   int    `env:"PORT,required"`
-	DBConn string `env:"DB_CONNECT"`
+	Port           int         `env:"PORT,required"`
+	DBConn         string      `env:"DB_CONNECT"`
+	ServiceName    string      `env:"SERVICE_NAME"`
+	JaegerEndpoint string      `env:"JAEGER_ENDPOINT"`
+	Environment    Environment `env:"ENVIRONMENT"`
 }
 
 func Load() (*Config, error) {
